b5b72e240ec03efebf06b93ed8bf1cb5854ab813
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2025-12-13 22:56:59 +0530

    Update startup.py

--- plugins/startup.py
+++ plugins/startup.py
@@ -1,11 +1,5 @@
 async def on_startup(bot):
     try:
-        text = (
-            " **X-OPTIMUS BOT STARTED** \n\n"
-            " System Online\n"
-            " Modules Loaded\n"
-            "Ready to Execute Commands"
-        )
-        await bot.send_message("me", text)
+        await bot.send_message("me", "X-OPTIMUS BOT STARTED | Online")
     except Exception as e:
         print(f"Startup message failed: {e}")
