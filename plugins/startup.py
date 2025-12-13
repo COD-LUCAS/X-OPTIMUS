9450f6c02839edd650fb5ebd52d22366e58df532
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2025-12-13 22:08:13 +0530

    Update startup.py

--- plugins/startup.py
+++ plugins/startup.py
@@ -1,10 +1,10 @@
 async def on_startup(bot):
     try:
         text = (
-            "🔥 **X-OPTIMUS BOT STARTED** 🔥\n\n"
-            "✅ System Online\n"
-            "⚙️ Modules Loaded\n"
-            "🚀 Ready to Execute Commands"
+            " **X-OPTIMUS BOT STARTED** \n\n"
+            " System Online\n"
+            " Modules Loaded\n"
+            "Ready to Execute Commands"
         )
         await bot.send_message("me", text)
     except Exception as e:
